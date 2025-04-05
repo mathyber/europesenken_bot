abd9f992c05905dca3feabc7ec7a299824452857
mathyber <ilan23f@gmail.com>
2025-04-05 03:55:44 +0300

    +

--- src/handlers/commands/webapp.js
+++ src/handlers/commands/webapp.js
@@ -2,7 +2,7 @@ const {webapp} = require("../../config/config");
 module.exports = async (ctx) => {
     await ctx.reply('Открыть Web-приложение:', {
         reply_markup: {
-            inline_keyboard: [
+            keyboard: [
                 [{
                     text: 'Запустить Web App',
                     web_app: webapp
