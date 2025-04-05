30a6a5bfda102fe5852991707439c8ad21cf19d4
mathyber <ilan23f@gmail.com>
2025-04-05 04:30:49 +0300

    +

--- src/handlers/actions/webAppData.js
+++ src/handlers/actions/webAppData.js
@@ -3,14 +3,9 @@ module.exports = async (ctx) => {
         console.log(ctx.webAppData.data.json())
         const data = ctx.webAppData.data.json();
         const userId = data.userId;
-        const message = data.message;
-        await ctx.replyWithPhoto(
-            { url: message },
-            {
-                caption: 'gg'
-            }
-        );
-        await ctx.reply(`Получено от Web App (${userId}): ${message}`)
+        const songs = data.songs;
+        console.log(songs);
+        await ctx.reply(`Получено от Web App (${userId})`)
     } catch (err) {
         console.error('Error processing web app data:', err);
         await ctx.reply('Произошла ошибка при обработке данных.');
